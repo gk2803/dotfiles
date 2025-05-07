af9e8883e1868193ca0c1be5a55cbfca5fcfb463
gk2803 <georkyri7@gmail.com>
2025-05-08 02:28:22 +0300

    math journalling workflow

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -39,7 +39,7 @@
  '(org-agenda-files
    '("~/Nextcloud/Documents/publicNotes/references/20250211T201751--agores.org"))
  '(package-selected-packages
-   '(org-agenda doom-themes ox-hugo python-pytest pyvenv direnv python-mode org-habit-stats gptel org-mode))
+   '(denote-journal org-agenda doom-themes ox-hugo python-pytest pyvenv direnv python-mode org-habit-stats gptel org-mode))
  '(package-vc-selected-packages '((org-mode :url "https://code.tecosaur.net/tec/org-mode"))))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
